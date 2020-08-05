0eeb9f2e6942cee5f4af2f4b8608abf0bd5120d4
Raphael Gomide <raphael@AVELL-RAPHAEL.localdomain>
2020-08-05 16:55:21 -0300

    done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,28 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return [...this.transactions];
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((accumulator, { type, value }) => {
+      const currentValue = type === 'income' ? value : 0;
+      return accumulator + currentValue;
+    }, 0);
+
+    const outcome = this.transactions.reduce((accumulator, { type, value }) => {
+      const currentValue = type === 'outcome' ? value : 0;
+      return accumulator + currentValue;
+    }, 0);
+
+    return { income, outcome, total: income - outcome };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const newTransaction = new Transaction({ title, value, type });
+    this.transactions.push(newTransaction);
+
+    return newTransaction;
   }
 }
 
